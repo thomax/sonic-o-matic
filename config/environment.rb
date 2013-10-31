d414691a03643098da0fdeb7c04d8fc6a83331ba
Thomas Drevon <thomas@bengler.no>
2013-10-31 10:01:36 +0100

    Monkeypatch array so we dont have to include activesupport

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,12 @@
 require 'yaml'
+
+# monkeypatch array to avoid requiring activesupport
+class Array
+  def extract_options!
+    last.is_a?(::Hash) ? pop : {}
+  end unless defined? Array.new.extract_options!
+end
+
 Dir[File.join(".", "lib/*.rb")].each {|f| require f}
 
 CONFIG = YAML.load_file('config/config.yml')
